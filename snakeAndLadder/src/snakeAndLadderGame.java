c65ff7636b4cdbfcf850f0b19815de9fe07cf98c
aDHIxx <sreemohan.adhithya@gmail.com>
2023-11-23 11:29:23 +0530

    Added:UseCase 2: Simulate rolling a dice

--- snakeAndLadder/src/snakeAndLadderGame.java
+++ snakeAndLadder/src/snakeAndLadderGame.java
@@ -1,13 +1,27 @@
 import java.util.Random;
 
 public class snakeAndLadderGame {
+
     /*@name: main
      *@desc: intialize the intial position of player to 0
      */
+    
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
-        int initialPos=0;
-        System.out.println("Player intial position is: "+initialPos);
-        System.out.println("Lets roll the dice");
+        int initialPos = 0;
+        System.out.println("Player initial position is: " + initialPos);
+
+        // Let's roll the dice using the rollDie method
+        int diceRoll = rollDie();
+        System.out.println("Dice Roll: " + diceRoll);
+    }
+
+    /*
+     * @name: rollDie
+     * @desc: Simulates rolling a six-sided die and returns the result.
+     */
+    private static int rollDie() {
+        Random random = new Random();
+        return random.nextInt(6) + 1; // Generate a random number between 1 and 6
     }
 }
