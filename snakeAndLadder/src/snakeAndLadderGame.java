61877ade45bd22c28c94c30b38c1b14d3d78ab0e
aDHIxx <sreemohan.adhithya@gmail.com>
2023-11-23 11:44:52 +0530

    Added: Usecase 3: Check option for no play, ladder or snake

--- snakeAndLadder/src/snakeAndLadderGame.java
+++ snakeAndLadder/src/snakeAndLadderGame.java
@@ -2,18 +2,21 @@ import java.util.Random;
 
 public class snakeAndLadderGame {
 
-    /*@name: main
-     *@desc: intialize the intial position of player to 0
-     */
-    
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
         int initialPos = 0;
         System.out.println("Player initial position is: " + initialPos);
 
-        // Let's roll the dice using the rollDie method
+        // roll the dice  --> rollDie() method
         int diceRoll = rollDie();
         System.out.println("Dice Roll: " + diceRoll);
+
+        // Check for Options No Play, Ladder, or Snake checkOption() method
+        int option = checkOption();
+        System.out.println("Option: " + option);
+
+        // Update player position based on the option
+        updatePosition(initialPos, diceRoll, option);
     }
 
     /*
@@ -24,4 +27,44 @@ public class snakeAndLadderGame {
         Random random = new Random();
         return random.nextInt(6) + 1; // Generate a random number between 1 and 6
     }
+
+    /*
+     * @name: checkOption
+     * @desc: Uses ((RANDOM)) to check for options.
+     *        Returns 0 -> No Play, 1 -> Ladder, 2 -> Snake.
+     */
+    private static int checkOption() {
+        Random random = new Random();
+        return random.nextInt(3); // Generate a random number between 0 and 2
+    }
+
+    /*
+     * @name: updatePosition
+     * @desc: Updates player position based on the option (No Play, Ladder, or Snake).
+     * @param: currentPosition, diceRoll, option
+     */
+    private static void updatePosition(int currentPosition, int diceRoll, int option) {
+        switch (option) {
+            case 0:
+                System.out.println("No Play.");
+                break;
+            case 1:
+                System.out.println("Ladder! Player moves ahead by " + diceRoll + " positions.");
+                currentPosition += diceRoll;
+                if (currentPosition > 100) {
+                    currentPosition = 100 - currentPosition; // Player cannot go above position 100
+                }
+                break;
+            case 2:
+                System.out.println("Snake! Player moves behind by " + diceRoll + " positions.");
+                currentPosition -= diceRoll;
+                if (currentPosition < 0) {
+                    currentPosition = 0; // Player cannot go below position 0
+                }
+                break;
+        }
+
+        System.out.println("New Position: " + currentPosition);
+    }
+
 }
